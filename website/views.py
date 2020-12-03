14435754e4a688ff4fd7dc2a415786db2771dca5
TAS-7 <1199116@students.wits.ac.za>
2020-12-03 23:17:34 +0200

    templates

--- website/views.py
+++ website/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
+# view for home page
 def home(request):
-	return HttpResponse('<h1> Home </h1>')
+	return render(request, 'website/home.html')
 
+
+# view for about page
 def about(request):
-	return HttpResponse('<h1> About </h1>')
+	return render(request, 'website/about.html')
 
-def login(request):
-	return HttpResponse('<h1>login </h1>')
 
-def registration(request):
-	return HttpResponse('<h1> registration')
 
 
